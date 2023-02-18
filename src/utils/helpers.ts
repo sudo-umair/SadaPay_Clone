bfa57d11c6815873547b3bf16940dc2b795072e4
Muhammad Umair <muhammadumair523@gmail.com>
2023-02-18 19:38:25 +0500

    updated helper.js

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -5,3 +5,11 @@ export const currencyFormatter = (value: number) => {
     minimumFractionDigits: 0,
   }).format(value);
 };
+
+export const phoneNumberFormatter = (value: string) => {
+  const countryCode = value.slice(0, 2);
+  const operator = value.slice(2, 5);
+  const number = value.slice(5, 13);
+
+  return `+${countryCode}-${operator}-${number}`;
+};
